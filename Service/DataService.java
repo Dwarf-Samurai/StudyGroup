924c07da3ec26bfd495ab4f70fdaf940edcf448f
Dwarf-Samurai <officer018@gmail.com>
2024-03-03 21:10:21 +0700

    Работа сделана

---
+++ Service/DataService.java
@@ -0,0 +1,10 @@
+package Service;
+
+import Model.User;
+
+import java.util.List;
+
+public interface DataService {
+    User create(User user);
+    List<User> read();
+}
---
+++ View/StudentView.java
@@ -0,0 +1,13 @@
+package View;
+
+import Model.Student;
+
+public class StudentView {
+    public void printStudent(Student student){
+        System.out.println("Студент: " + student);
+    }
+    public void printAllStudent(){
+
+    }
+
+}
